e84f4fe662b8fbe923050a8def5f11820b090839
KotAtWork <Koti4Matroskin@gmail.com>
2020-09-07 11:52:33 +0300

    update db config

--- config/product_settings.py
+++ config/product_settings.py
@@ -15,8 +15,12 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'tmvdb.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tmvsite',
+        'USER': 'vitalita',
+        'PASSWORD': 'setupmymind',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
