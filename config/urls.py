3bd12261d3040212449e7152ea4b39765a22f530
KotAtWork <Koti4Matroskin@gmail.com>
2020-09-08 08:43:17 +0300

    urlpatterns for debug true

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     # path('pages/', include('django.contrib.flatpages.urls')),
 ]
 
-if settings.DEBUG += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
               # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
